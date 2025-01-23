9624263ac156e47bb8df726356338c02fcb493fe
Thiago <thiagorafael2005@gmail.com>
2025-01-23 02:51:34 -0300

    recomecando certo

---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <div className="flex bg-red-500 justify-center gap-72">
+      <a href="">asdasdasd</a>
+      <a href="">123123123</a>
+      <a href="">asdasdasd</a>
+      <a href="">12312312123</a>
+    </div>
+  );
+};
