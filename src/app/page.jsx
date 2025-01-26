974b2189f5ce90f6bfbe9a4470a00b09e794ba4b
Thiago <thiagorafael2005@gmail.com>
2025-01-25 22:22:41 -0300

    inicio feito

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,51 @@
+"use client";
+
 import { Header } from "@/components/header";
-import Image from "next/image";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 export default function Home() {
+  const HeaderRef = useRef(null);
+  const NameRef = useRef(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline({ defaults: { duration: 1 } });
+    const header = HeaderRef.current;
+    const name = NameRef.current;
+
+    tl.fromTo(
+      header,
+      {
+        opacity: 0,
+        y: -100,
+      },
+      { opacity: 1, y: 0 }
+    ).fromTo(
+      name,
+      {
+        opacity: 0,
+        y: -100,
+      },
+      { opacity: 1, y: 0 }
+    );
+  }, []);
+
   return (
     <div>
-      <Header />
+      <div ref={HeaderRef}>
+        <Header />
+      </div>
+      <div
+        className="flex flex-col items-center justify-center gap-7 mt-48"
+        ref={NameRef}
+      >
+        <h2 className="text-black text-8xl font-comfortaa text-center ">
+          Thiago Rafael
+        </h2>
+        <p className="font-comfortaa text-3xl text-gray-400">
+          Desenvolvedor FullStack
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,38 @@
+import Image from "next/image";
+import LogoThiago from "@/assets/image/LogoThiago.png";
+import { Button } from "./ui/button";
+
 export const Header = () => {
   return (
-    <div className="flex bg-red-500 justify-center gap-72">
-      <a href="">asdasdasd</a>
-      <a href="">123123123</a>
-      <a href="">asdasdasd</a>
-      <a href="">12312312123</a>
+    <div className="text-black flex justify-center gap-96 font-comfortaa items-center mt-6">
+      <Image src={LogoThiago} width={80} alt="Logo do Thiago Rafael" />
+      <div className="flex gap-6">
+        <a
+          href=""
+          className="hover:text-gray-400 hover:underline transition-all delay-75 "
+        >
+          Projects
+          <span></span>
+        </a>
+        <a
+          href=""
+          className="hover:text-gray-400 hover:underline transition-all delay-75"
+        >
+          Skills
+        </a>
+        <a
+          href=""
+          className="hover:text-gray-400 hover:underline transition-all delay-75"
+        >
+          About Me
+        </a>
+      </div>
+      <Button
+        className="bg-transparent border border-black  text-black rounded-lg font-comfortaa hover:bg-black transition-all hover:text-white"
+        variant="outline"
+      >
+        Contact
+      </Button>
     </div>
   );
 };
