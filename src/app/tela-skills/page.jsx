c689aafeffd22a73ddc0f7a1f58a6d3ad90fe272
Thiago <thiagorafael2005@gmail.com>
2025-01-28 17:57:40 -0300

    arrumando erro de importação do header

--- src/app/tela-skills/page.jsx
+++ src/app/tela-skills/page.jsx
@@ -1,4 +1,4 @@
-import { Header } from "@/components/Header";
+import { Header } from "@/components/header";
 import { Skills } from "@/components/Skills";
 
 export default function SkillsThiago() {
